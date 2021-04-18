84035848d271ae8422dc51745223c301a7696d90
Guglnora <g.smadiiarova@yandex.ru>
2021-04-18 15:36:10 -0400

    new

---
+++ add_to_cart.js
@@ -0,0 +1,54 @@
+//2) По клику на кнопку лобавить в карзину товар с след паметрами:
+  //1) Картинка 
+  // 2) Заголовок
+  // 3) Доп параметры цвет
+   //4) Цена
+//3) Добавить в карзину
+// Конкретные задачи
+ //1) Получить картинку
+ //2) 1) Получить Заголовок
+  // Решение:
+//  let ttl = document.getElementsByClassName("ttl");
+//  ttl[1].innerText
+
+ //3) Получить доп параметры
+ //4) полученить Цена
+
+// Принцип получения данные следующее:
+// Получить данные можно с помощю
+ //.class
+ //#ad
+// attribute
+ //tag
+
+
+ // .class - document.getElementsByClassName - внутри елемента получить элементы по названии класса
+//          вкладка
+//          (.) связка
+
+// get получить
+//  elements - Элементы (потому что классов бывает  нексколько)
+// By - по/ ОТ
+ 
+// ByClassName - название класса
+//
+//
+//
+//
+
+// let ttl = document.getElementsByClassName("ttl");
+// ttl[1].innerText
+// ничег не вывилась
+
+// Без команды ничегоне выводиться
+
+
+let ttl = document.getElementsByClassName("ttl");
+
+
+console.log(ttl[1].innerText);
+
+let img = document.getElementsByClassName("img");
+
+
+console.log(img[1].innerText);
